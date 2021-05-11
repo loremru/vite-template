7e2d11389c4046c3fb367f60df4ce051f350fb37
Issakov Ilya <mr.nonamerz08@yandex.ru>
2021-05-11 20:26:39 +0600

    typescript support, vue-router

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,18 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/prettier'],
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint',
+  ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     semi: ['error', 'never'],
+    '@typescript-eslint/ban-ts-comment': 0,
     'prettier/prettier': ['error', { singleQuote: true, semi: false }],
     quotes: ['error', 'single'],
   },
--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,15 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 // @ts-ignore
 import path from 'path'
-import eslintPlugin from "vite-plugin-eslint";
-
+import eslintPlugin from 'vite-plugin-eslint'
 
 const eslintOptions = {
   include: ['./**/*.vue', './**/*.js', './**/*.ts'],
-  exclude: ['node_modules']
+  exclude: ['node_modules'],
 }
+// @ts-ignore
+
+console.log('DI!!!RRR', path.resolve(__dirname, 'src'))
 
 export default defineConfig({
   plugins: [vue(), eslintPlugin(eslintOptions)],
@@ -17,15 +19,15 @@ export default defineConfig({
       {
         find: '@',
         // @ts-ignore
-        replacement: path.resolve(__dirname, 'src')
-      }
-    ]
+        replacement: path.resolve(__dirname, 'src'),
+      },
+    ],
   },
   css: {
     preprocessorOptions: {
       scss: {
         additionalData: '@import "@/assets/styles/_variables.scss";',
-      }
-    }
-  }
+      },
+    },
+  },
 })

c805d38cb4b30f80b31b132bf14836f25daf23d6
Issakov Ilya <mr.nonamerz08@yandex.ru>
2021-05-11 19:37:24 +0600

    base template

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+// @ts-ignore
+import path from 'path'
+import eslintPlugin from "vite-plugin-eslint";
+
+
+const eslintOptions = {
+  include: ['./**/*.vue', './**/*.js', './**/*.ts'],
+  exclude: ['node_modules']
+}
+
+export default defineConfig({
+  plugins: [vue(), eslintPlugin(eslintOptions)],
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        // @ts-ignore
+        replacement: path.resolve(__dirname, 'src')
+      }
+    ]
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@import "@/assets/styles/_variables.scss";',
+      }
+    }
+  }
+})
